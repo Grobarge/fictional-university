c8f07fb89d2cae52afcd5977764fb9ade39904e6
Gaje Robarge <gajer@paulmitchell.edu>
2019-09-27 13:41:01 -0600

    Updated the header.php with some more info on the site

--- fictional-university-theme/header.php
+++ fictional-university-theme/header.php
@@ -5,6 +5,16 @@
     </head>
         <body>
             <h1>Fictional University</h1>
+            <div>
+                <h2>This will be for practice, ya know</h2>
+                <ul>
+                    <li>Practice 1 </li>
+                    <li>Practice 2</li>
+                    <li> Practice Jerry</li>
+                    <li>Practice Bobby</li>
+                </ul>
+            
+            </div>
         </body>
 
 </html>

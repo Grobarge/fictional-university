f86973aa48d4ccf3a1a73fe0f3655b34544efb65
Gaje Robarge <gajer@paulmitchell.edu>
2019-10-02 15:31:13 -0600

    added dynamic functionality for bredcrumbs on single pages.

--- fictional-university-theme/page.php
+++ fictional-university-theme/page.php
@@ -18,9 +18,19 @@ while (have_posts()) {
 
     <div class="container container--narrow page-section">
 
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-        </div>
+
+        <!-- this chunck of code below affects the breadcrumb on the pages and dynamicly switches the text on the breadcrumb based off of the page its on -->
+        <?php
+            // if the id of the post is a child to the parent id then display the title of parent page on breadcrumb if its parent page, dont display bredcrumb //
+            $theParentVariable = wp_get_post_parent_id(get_the_ID());
+            if ($theParentVariable) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentVariable); ?>"><i class="fa fa-home" aria-hidden="true"></i><?php echo get_the_title($theParentVariable); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+            </div>
+        <?php
+            }
+            ?>
+
 
         <!-- <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Us</a></h2>
